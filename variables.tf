1a9159f9de67da511743b4a5a2f93a176f184cb8
Mohammed Daaniyaal <35423314+iamdaaniyaal@users.noreply.github.com>
2019-10-17 18:28:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,5 @@ variable "sonarvpc" {}
 variable "sonarsub" {}
 // variable "elk_instance_name" {}
 variable "instance_name" {}
+variable "maven_instance_ip_name" {}
+variable "maven_instance_ip_region" {}
